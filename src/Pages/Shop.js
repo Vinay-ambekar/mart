a9bc3ab654e1992f988186110ed32004516eeb91
vinay <vinayambekar.b@gmail.com>
2024-05-22 20:04:06 +0530

    'shop_page_done'

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,62 +1,121 @@
-import { Link } from "react-router-dom";
 import { products } from "../DataPath/products";
 import Headerimage from "../components/Headerimage";
-import { Card, CardContent, CardMedia, Rating } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
 import SingleCard from "../components/SingleCard";
+import { useEffect, useRef, useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
 
-const Shop=()=>{
-    return(
-        <>
-        <Headerimage porduct={'product'}/>
-        <div className="container-fluid pb-5 d-flex justify-content-center home">
+const Shop = () => {
+  const [selectedItem, setSelectedItem] = useState("Filter By Category");
+  const [listtoggle, setlisttoggle] = useState(false);
+  const [searchvalue, setsearchvalue] = useState("");
+  const [searchtoggle, setsearchtoggle] = useState(false);
+  const dropdownRef = useRef(null);
+  /*  console.log("selecte item>>", selectedItem); */
+
+  const items = ["sofa", "chair", "mobil", "watch", "wireless"];
+
+  const handeltoggle = (event) => {
+    event.stopPropagation();
+    setlisttoggle(!listtoggle);
+  };
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setlisttoggle(false);
+    setsearchvalue("");
+    setsearchtoggle(false);
+  };
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setlisttoggle(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+  const handerchange = (e) => {
+    /*     console.log(e.target.value) */
+    setsearchvalue(e.target.value);
+    if (e.target.value.length > 0) {
+      setsearchtoggle(true);
+    } else if (e.target.value.length === 0) {
+      setsearchtoggle(false);
+    }
+  };
+
+  return (
+    <>
+      <Headerimage porduct={"product"} />
+      <div className="selectSearch">
+        {/* selection and serch bar */}
+        <div className="container my-5 ">
+          <div className="row">
+            <div className="col-xs-12 col-md-12 col-lg-5 mb-5">
+              <div ref={dropdownRef}>
+                <div
+                  className="fs-5 select-btn d-flex justify-content-between align-items-center p-2"
+                  onClick={handeltoggle}
+                >
+                  <span>{selectedItem || "Filter By Category"}</span>
+                  <KeyboardArrowDownIcon className="ps-2 fs-2 downarrow" />
+                </div>
+                <ul className={` ${listtoggle ? "d-flex-column" : "d-none"}`}>
+                  {items.map((item, index) => (
+                    <li
+                      onClick={() => handleItemClick(item)}
+                      key={index}
+                      value={item}
+                      className={`${item === selectedItem ? "selected" : ""}`}
+                    >
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="col-xs-12 col-md-12 col-lg-7">
+              <div className="serch d-flex justify-content-between align-items-center me-5">
+                <input
+                  className="w-100 p-2 fs-5 ms-3"
+                  type="text"
+                  autoComplete="off"
+                  name="search"
+                  value={searchvalue}
+                  placeholder="Search...."
+                  onChange={handerchange}
+                />
+                <SearchIcon className="searchicon mx-2" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* product for filter */}
+        <div
+          className={`container-fluid pb-5  justify-content-center  home ${
+            searchtoggle ? "d-none" : "d-flex"
+          } `}
+        >
           <div className={`row w-75 `}>
             {products.length > 0 ? (
-              products.filter(product=> ['mobile', 'wireless'].some(category => product.category.includes(category))).map((product) => (
-                <div
-                  className=" col-sm-12 col-md-6 col-lg-6 col-xl-4 "
-                  key={product.id}
-                >
-                    <SingleCard product={product}/>
-{/*                   <div>
-                    <Card
-                      sx={{
-                        maxWidth: "30rem",
-                        maxHeight: "50rem",
-                        margin: "1rem auto",
-                        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.5)",
-                      }}
-                    >
-                     <Link className="z-3" to={`/${product.id}`}> 
-                      <CardMedia
-                        component="img"
-                        sx={{
-                          height: 200,
-                          objectFit: "contain",
-                          margin: "0.5rem",
-                        }}
-                        src={product.imgUrl}
-                        alt="A Random picture found with the keyword Tokyo"
-                      />
-                      </Link>
-                      <CardContent>
-                        <p className="fs-4 fw-medium">{product.productName}</p>
-                        <div>
-                          <Rating name="read-only" value={+product.reviews[0].rating} readOnly />
-                        </div>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <p className="fs-3 fw-bold mt-2">
-                            {product.price}$
-                          </p>
-                          <span className="p-2 addproduct">
-                            <AddIcon/>
-                          </span>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  </div> */}
-                </div>
-              ))
+              products
+                .filter((product) =>
+                  [
+                    selectedItem == "Filter By Category"
+                      ? "sofa"
+                      : selectedItem,
+                  ].some((category) => product.category.includes(category))
+                )
+                .map((product) => (
+                  <div
+                    className=" col-sm-12 col-md-6 col-lg-6 col-xl-4 "
+                    key={product.id}
+                  >
+                    <SingleCard product={product} />
+                  </div>
+                ))
             ) : (
               <center className="mt-5">
                 <button className="btn btn-success w-50" type="button" disabled>
@@ -66,7 +125,47 @@ const Shop=()=>{
             )}
           </div>
         </div>
-        </>
-    )
-}
+        {/* product for search */}
+        <div
+          className={`container-fluid pb-5  justify-content-center  home ${
+            searchtoggle ? "d-flex" : "d-none"
+          } `}
+        >
+          <div className={`row w-75 `}>
+            {products.length > 0 ? (
+              products.filter((product) =>
+                product.productName
+                  .toLowerCase()
+                  .includes(searchvalue.toLowerCase())
+              ).length === 0 ? (
+                <p className="fs-1 fw-medium text-center">Product Not Found !!</p>
+              ) : (
+                products
+                  .filter((product) =>
+                    product.productName
+                      .toLowerCase()
+                      .includes(searchvalue.toLowerCase())
+                  )
+                  .map((product) => (
+                    <div
+                      className="col-sm-12 col-md-6 col-lg-6 col-xl-4"
+                      key={product.id}
+                    >
+                      <SingleCard product={product} />
+                    </div>
+                  ))
+              )
+            ) : (
+              <center className="mt-5">
+                <button className="btn btn-success w-50" type="button" disabled>
+                  <span className="sr-only">Loading...</span>
+                </button>
+              </center>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Shop;
--- src/Pages/cart.js
+++ src/Pages/cart.js
@@ -1,7 +1,23 @@
 const Cart=()=>{
     return(
         <>
-        <h2>Cart</h2>
+        <div className="container-fluid cartpage">
+            <div className="row">
+            <div className="col-xs-12 col-md-12 col-lg-8">
+                <div className="card  mt-5 mb-3 ">
+                <p className="text-center">Your cart is empty</p>
+                </div> 
+            </div>
+            <div className="col-xs-12 col-md-12 col-lg-4">
+            <div className="card  mt-5 mb-3">
+                <span className="ms-3 mt-2 fs-4 fw-medium cartsummary">Cart summary</span>
+                <hr className="mx-3"/>
+                <span className="ms-3 mt-2 fs-6">Total Price:</span>
+                <span className="ms-3 mt-2 mb-3 fs-2 fw-medium cartsummary">$0.00</span>
+                </div> 
+            </div>
+            </div>
+        </div>
         </>
     )
 }
